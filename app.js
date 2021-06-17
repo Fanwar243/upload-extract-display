71f38590a41ea02a83ad137b9ccf0f0227dc737b
Farid Anwar <faridanwar243@gmail.com>
2021-06-17 15:46:07 +0800

    Error catching

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 async function extract() {
     file = document.getElementById("file").files[0];
-    const reader = new zip.ZipReader(new zip.BlobReader(file));
-    const entries = await reader.getEntries();
-    let images = [];
-    if (entries.length === 5) {
-        for (i=0; i < entries.length; i++) {
-            const image = await entries[i].getData(
-                new zip.BlobWriter("image/jpeg")
-            );
-            const blobUrl = URL.createObjectURL(image);
-            images.push(blobUrl);
+    let imagePlaceholders = [
+        document.getElementById("image1"),
+        document.getElementById("image2"),
+        document.getElementById("image3"),
+        document.getElementById("image4"),
+        document.getElementById("image5")]
+    try {
+        const reader = new zip.ZipReader(new zip.BlobReader(file));
+        const entries = await reader.getEntries();
+        let images = [];
+        if (entries.length === 5) {
+            for (i=0; i < entries.length; i++) {
+                const image = await entries[i].getData(
+                    new zip.BlobWriter("image/jpeg")
+                );
+                const blobUrl = URL.createObjectURL(image);
+                images.push(blobUrl);
+            }
+            document.getElementById("noImage").style.display = "none";
+            for (i=0; i < imagePlaceholders.length; i++) {
+                imagePlaceholders[i].src = images[i];
+            }
         }
-        document.getElementById("noImage").style.display = "none";
-        let image1 = document.getElementById("image1");
-        let image2 = document.getElementById("image2");
-        let image3 = document.getElementById("image3");
-        let image4 = document.getElementById("image4");
-        let image5 = document.getElementById("image5");
-        image1.src = images[0];
-        image2.src = images[1];
-        image3.src = images[2];
-        image4.src = images[3];
-        image5.src = images[4];
+        else {
+            document.getElementById("tooBig").style.display = "block";
+        }
+        await reader.close();
+    } catch(err) {
+        console.log(err);
+        document.getElementById("error").style.display = "block";
     }
-    else {
-        document.getElementById("tooBig").style.display = "block";
-    }
-    await reader.close();
+    
 }
